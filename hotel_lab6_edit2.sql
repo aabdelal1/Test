30c1d0a64e398c7c4726cb3e48a2c7879f9aa0ec
Aly <alymalfateh@gmail.com>
2024-12-13 15:31:54 +0200

    Added embloyees table

---
+++ hotel_lab6_edit2.sql
@@ -0,0 +1,8 @@
+select * From guest;
+select * From room;
+alter table room ADD age int;
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT
+);
